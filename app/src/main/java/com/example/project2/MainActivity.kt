3f5d45e2fe9c4fa7766e283f1710b4640596e8be
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-18 06:47:58 +0700

    manambahkan background color, dan memodifikasi minsdk

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.project2.ui.theme.Project2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,16 +26,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             Project2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BasicCompose(jamal = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(jamal:Modifier = Modifier){
+    Column  (verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
+            .padding(top =50.dp)
+            .background(Color(0xFFF2F0EB)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ){}
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
