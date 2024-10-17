7f76b0cb57ad74d85d6953c4781302db86371ed1
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-18 06:51:43 +0700

    menambahkan style text login = Bold

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +47,7 @@ fun BasicCompose(jamal:Modifier = Modifier){
             text = "Login",
             fontSize = 50.sp,
             color = Color(0xFF708090),
+            fontWeight = FontWeight.Bold
         )
     }
 }
