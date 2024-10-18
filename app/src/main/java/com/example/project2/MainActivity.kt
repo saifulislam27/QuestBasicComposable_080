870677f7b8c690c9f3e2e19d20cdf5c8590575c5
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-18 07:22:18 +0700

    menambahkan clip untuk mengatur shape corner di elemen gambar image3

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -90,6 +93,7 @@ fun BasicCompose(jamal:Modifier = Modifier){
             modifier = Modifier
                 .size(350.dp)
                 .padding(top = 16.dp)
+                .clip(RoundedCornerShape(500.dp))
         )
     }
 }
